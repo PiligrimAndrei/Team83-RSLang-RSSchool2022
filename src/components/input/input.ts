38ac99871b5e53726d1382694761592030accfc4
Danila.Halm <hltv.daniel@gmail.com>
2022-08-20 13:33:26 +0300

    feat: add common input component

---
+++ src/components/input/input.ts
@@ -0,0 +1,21 @@
+import { Component } from '../../components/components';
+import './input.css'
+export class Input extends Component {
+    
+    constructor(
+        parentNode:HTMLElement,
+        type:string,
+        styles: string[] = [],
+        placeholder:string,
+        autofocus: Boolean = false
+        ){
+        super(parentNode, 'input');
+
+        this.element.setAttribute("type",type);
+        this.element.setAttribute("placeholder",placeholder);
+        this.element.classList.add(...styles);
+        if(autofocus) {
+            this.element.setAttribute("autofocus",autofocus);
+        }
+    }
+}
