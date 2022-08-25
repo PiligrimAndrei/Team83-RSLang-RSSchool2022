e111fe95e568a8ee27a4304311a8074acdcf3981
Andrei Krasko <andrei@krasko.by>
2022-08-25 17:18:23 +0300

    feat: add gameSprint.ts and api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,10 @@
-import { IWord } from '../interfaces/interfaces'
+import { IWord, IUser, IUpdateUser, IUserWord, ISignIn } from '../interfaces/interfaces'
 
-const baseURL = 'https://team83-rslang.herokuapp.com';
+const baseUrl = 'https://team83-rslang.herokuapp.com';
 
 export const getWords = async (group = 0, page = 0,): Promise<{ words: Array<IWord> } | null> => {
   try {
-    const data = await fetch(`${baseURL}/words?group=${group}&page${page}`);
+    const data = await fetch(`${baseUrl}/words?group=${group}&page${page}`);
     const res: IWord[] = await data.json()
 
     if (data.status === 200) {
@@ -20,7 +20,7 @@ export const getWords = async (group = 0, page = 0,): Promise<{ words: Array<IWo
 
 export const getWord = async (wordID: string): Promise<IWord | null> => {
   try {
-    const data = await fetch(`${baseURL}/words/${wordID}`);
+    const data = await fetch(`${baseUrl}/words/${wordID}`);
     const res: IWord = await data.json()
 
     if (data.status === 200) {
@@ -32,3 +32,79 @@ export const getWord = async (wordID: string): Promise<IWord | null> => {
   }
 }
 
+export const getUser = async (UserID: string): Promise<IUser | null> => {
+  try {
+    const data = await fetch(`${baseUrl}/users/${UserID}`);
+    const res: IUser = await data.json()
+
+    if (data.status === 200) {
+      return res
+    }
+    if (data.status === 404) {
+      return res // TO DO  User not found
+    }
+    if (data.status === 401) {
+      return res // TO DO  	Access token is missing or invalid
+    }
+    return null;
+
+  } catch (err) {
+    throw new Error("Error");
+  }
+}
+export const createUser = async (user: IUser): Promise<void> => {
+  try {
+    await fetch(`${baseUrl}/user`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    });
+
+  }
+  catch (err) {
+    throw new Error("Error");
+  }
+}
+
+export const deleteUser = async (userID: string): Promise<void> => {
+  try {
+    await fetch(`${baseUrl}/user/${userID}`, {
+      method: 'DELETE',
+    });
+  }
+  catch (err) {
+    throw new Error("Error");
+  }
+}
+export const updateUser = async (user: IUpdateUser): Promise<void> => {
+  try {
+    await fetch(`${baseUrl}/user/${user.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+export const signIn = async (user: IUpdateUser): Promise<ISignIn | null> => {
+  const data = await fetch(`${baseUrl}/signin`,
+    {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    });
+  const res: ISignIn = await data.json();
+  if (data.status === 200) {
+    return res
+  }
+  return null;
+}
--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,5 +1,5 @@
 export interface IWord {
-  is: string;
+  id: string;
   group: 0;
   page: 0;
   word: string;
@@ -14,7 +14,35 @@ export interface IWord {
   textMeaningTranslate: string;
   textExampleTranslate: string;
 }
+
+export interface IUser {
+  id: string;
+  name: string;
+  email: string;
+}
+export interface IUpdateUser {
+  id?: string;
+  name: string;
+  email: string;
+}
+export interface IUserWord {
+  difficulty: string;
+  optional:
+  {
+    counter: number,
+    correct: number,
+    uncorrect: number
+  }
+}
 export interface IRoute {
   name: string;
   component: () => void;
+}
+
+export interface ISignIn {
+  message: string;
+  token: string;
+  refreshToken: string;
+  userID: string;
+  name: string;
 }
