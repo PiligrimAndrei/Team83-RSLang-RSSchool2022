e5de51e44ced3427c32ac813e6874557c9e87105
Andrei Krasko <andrei@krasko.by>
2022-09-01 18:13:47 +0300

    feat: add startWindow

--- src/pages/gameSprint/gameSprint.ts
+++ src/pages/gameSprint/gameSprint.ts
@@ -1,10 +1,28 @@
+import "./gameSprint.css";
 import { Component } from "../../components/components";
 import { GameSprint } from "../../components/gameSprint/gameSprintComponents";
+import { Link } from "../../components/link/link";
+import { SprintGameDescription } from '../../components/sprintGameDescription/sprintGameDescription'
 export class GameSprintPage extends Component {
-  private gameSprintPage: Component;
+  //private gameSprintPage: Component;
+  private SprintGameStartWrapper: Component;
+  private SprintGameCloseLink: Link;
+  private startButton: HTMLElement | null;
   constructor(parentNode: HTMLElement) {
-    super(parentNode, 'div', ['game_sprint_page']);
+    super(parentNode, 'div', ['gameSprint']);
 
-    this.gameSprintPage = new GameSprint(this.element);
+    this.SprintGameCloseLink = new Link(
+      this.element,
+      "#/games",
+      ["SprintGameCloseLink"],
+      "X"
+    );
+
+    // this.gameSprintPage = new GameSprint(this.element);
+
+    this.SprintGameStartWrapper = new SprintGameDescription(this.element);
+
+    this.startButton = this.SprintGameStartWrapper.element
+      .lastElementChild as HTMLElement;
   }
 }
